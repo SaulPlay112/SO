d4fc9dea840b1dbca5a4fead315b9b0f3cd28372
SaulPlay112 <saulplay112004@gmail.com>
2024-09-22 00:14:17 -0600

    se genero el proyecto

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef struct snode {
+    string mensaje;
+    snode *id;
+    snode *tiempo;
+    snode *prioridad;
+} dato;
+
+void asigna(dato &n, dato *id, dato *tiempo, dato *prioridad) {
+    n.id = id;
+    n.tiempo = tiempo;
+    n.prioridad = prioridad;
+}
+int main() {
+    cout<<"Ingresa una opcion: \n";
+    cout<<"FIFO-[1]\n";
+    cout<<"SJF-[2]\n";
+    cout<<"R.R-[3]\n";
+    cout<<"Prioridad-[4]\n";
+    cout<<"Salir-[0]\n";
+    int opcion;
+    cin>>opcion;
+    switch (opcion) {
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+    }
+    return 0;
+}
+
+
+// funciones para los tiempos
+
+void fifo() {
+
+}
+void sjf() {
+
+}
+void r_r() {
+
+}
+void prioridad() {
+
+}
