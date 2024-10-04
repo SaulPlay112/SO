8c90fd7b772fcd145426f3857474d7142410b27e
Saul <saulplay112004@gmail.com>
2024-10-03 18:29:49 -0600

    se borro todo el codigo para poder empezar de nuevo utilizando listas enlazadas para poder manejar mejor los datos... tambien programe la estructura default de la lista

--- main.cpp
+++ main.cpp
@@ -1,118 +1,15 @@
 #include <iostream>
-#include <string>
 using namespace std;
-//se creo la estructura con los datos inicializados
-//Si ves este mensaje Saul significa que ya funciona las actualizaciones (mandame mensaje si ves esto para confirmarlo )
-struct dato {
+
+// SAUL- Cree la estructura de la lista enlazada con lo que vamos a ocupar...  id (proceso), tiempo(tiempo del proceso), prioridad(prioridad del proceso)
+struct nodo {
     int id;
-    int tiempo;
-    int prioridad;
+    int t;
+    int p;
+    nodo* siguiente;
+
+    // SAUL- cree el constructor para utilizar la lista enlazada para los procesos
+    nodo(int proceso, int tiempo , int prioridad) : id(proceso), t(tiempo), p(prioridad), siguiente(nullptr) {}
 };
-// datos generales apuntando a la estructura
-void datosGenerales(dato* n, int id, int tiempo, int prioridad) {
-    n -> id = id;
-    n -> tiempo = tiempo;
-    n -> prioridad = prioridad;
-}
-// inicializar las funciones (metodos)
-void datosGenerales(dato* n, int id, int tiempo, int prioridad);
-void agregarDatos();
-void fifo();
-void sjf();
-void r_r();
-void prioridad();
 
-int main() {
-    int opcion;
-    do{
-    cout<<"Ingresa una opcion: \n";
-    cout<<"Ingresar valores-[1]\n";
-    cout<<"FIFO-[2]\n";
-    cout<<"SJF-[3]\n";
-    cout<<"R.R-[4]\n";
-    cout<<"Prioridad-[5]\n";
-    cout<<"Salir-[0]\n";
-    cin>>opcion;
-    switch (opcion) {
-        case 1:
-             agregarDatos();
-        break;
-        case 2:
-             fifo();
-        break;
-        case 3:
-            sjf();
-        break;
-        case 4:
-            r_r();
-        break;
-        case 5:
-            prioridad();
-        break;
-    }
-}while (opcion != 0);
-    return 0;
 
-}
-// funciones para los tiempos
-
-void fifo() {
-}
-
-void sjf() {
-}
-
-void r_r() {
-}
-
-void prioridad() {
-}
-void agregarDatos() {
-    cout<<"FIFO-[1]\n";
-    cout<<"SJF-[2]\n";
-    cout<<"R.R-[3]\n";
-    cout<<"Prioridad-[4]\n";
-    int opcion;
-    cin>>opcion;
-if(opcion==1 || opcion==2 || opcion==3) {
-    int a;
-    cout<<"Ingresa los procesos: \n";
-    cin>>a;
-
-    dato* node = new dato[a];
-    for (int i = 0; i < a; i++) {
-        int id, tiempo;
-        cout<<"Ingresa el id del proceso: "<< i + 1<<": ";
-        cin>>id;
-        cout<<"Ingresa el tiempo del proceso: "<< i + 1<<": ";
-        cin>>tiempo;
-        datosGenerales(&node[i], id, tiempo,0);
-    }
-    cout << "\nProcesos ingresados: \n";
-    for (int i = 0; i < a; i++) {
-        cout << "ID: " << node[i].id << ", Tiempo: " << node[i].tiempo << endl;
-    }
-    delete[] node;
-}else if (opcion == 4) {
-    int a;
-    cout<<"Ingresa los procesos: \n";
-    cin>>a;
-
-    dato* node = new dato[a];
-    for (int i = 0; i < a; i++) {
-        int id, tiempo, prioridad;
-        cout<<"Ingresa el id del proceso: "<< i + 1<<": ";
-        cin>>id;
-        cout<<"Ingresa el tiempo del proceso: "<< i + 1<<": ";
-        cin>>tiempo;
-        cout<<"Ingresa la prioridad de: "<< i + 1<< ": ";
-        cin>>prioridad;
-        datosGenerales(&node[i], id, tiempo, prioridad);
-    }
-    cout << "\nProcesos ingresados: \n";
-    for (int i = 0; i < a; i++) {
-        cout << "ID: " << node[i].id << ", Tiempo: " << node[i].tiempo <<", Prioridad: "<< node[i].prioridad<<endl;
-    }
-    delete[] node;
-}
-}
