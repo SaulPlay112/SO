4f370ed10d3a54dc51d510ae29d41fcfa68b3d54
TamlitoVerdeMain <za230110877@zapopan.tecmm.edu.mx>
2024-10-28 10:38:28 -0600

    ultima prueba alv

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> // Para std::min
 using namespace std;
 
 // Estructura del nodo
@@ -7,17 +8,19 @@ struct nodo {
     int id;
     int t; // Tiempo restante
     int p; // Prioridad
-    int t_llegada; // Para manejar el tiempo de llegada en Round Robin
+    int t_llegada; // Tiempo de llegada para Round Robin
     nodo* siguiente;
 
-    nodo(int proceso, int tiempo, int prioridad) : id(proceso), t(tiempo), p(prioridad), t_llegada(0), siguiente(nullptr) {}
+    nodo(int proceso, int tiempo, int prioridad)
+        : id(proceso), t(tiempo), p(prioridad), t_llegada(0), siguiente(nullptr) {}
 };
 
+// Declaración de funciones
 void ejecutarYRemoverProceso(nodo*& cabeza, nodo* proceso);
 nodo* seleccionarMayorPrioridad(nodo* cabeza);
 
 // Función para agregar nodos a la lista
-void agregarnodo(nodo*& cabeza, int id, int tiempo, int prioridad) {
+void agregarNodo(nodo*& cabeza, int id, int tiempo, int prioridad) {
     nodo* nuevoNodo = new nodo(id, tiempo, prioridad);
 
     if (cabeza == nullptr) {
@@ -50,20 +53,20 @@ void imprimirLista(nodo* cabeza) {
 
     nodo* temp = cabeza;
     while (temp != nullptr) {
-        cout << "ID: " << temp->id << ", Tiempo: " << temp->t << ", Prioridad: " << temp->p << "\n" << endl;
+        cout << "ID: " << temp->id << ", Tiempo: " << temp->t << ", Prioridad: " << temp->p << "\n";
         temp = temp->siguiente;
     }
+    cout << endl;
 }
 
 // Función para pedir datos
-void pedirdatos(vector<vector<int>>& datos) {
+void pedirDatos(vector<vector<int>>& datos) {
     int cantidadNodos;
-
     cout << "Ingrese la cantidad de nodos: ";
     cin >> cantidadNodos;
-    for (int i = 0; i < cantidadNodos; i++) {
-        int id, tiempo, prioridad;
 
+    for (int i = 0; i < cantidadNodos; ++i) {
+        int id, tiempo, prioridad;
         cout << "Ingrese los datos para el nodo " << (i + 1) << ":\n";
         cout << "ID: ";
         cin >> id;
@@ -71,18 +74,19 @@ void pedirdatos(vector<vector<int>>& datos) {
         cin >> tiempo;
         cout << "Prioridad: ";
         cin >> prioridad;
+
         datos.push_back({id, tiempo, prioridad});
     }
 }
 
 // Función para transferir datos del vector a la lista enlazada
-void tranferirDatos(vector<vector<int>>& datos, nodo*& cabeza) {
-    for (auto& a : datos) {
-        agregarnodo(cabeza, a[0], a[1], a[2]);
+void transferirDatos(const vector<vector<int>>& datos, nodo*& cabeza) {
+    for (const auto& a : datos) {
+        agregarNodo(cabeza, a[0], a[1], a[2]);
     }
 }
 
-// Función para calcular el promedio de los tiempos
+// Función para calcular el promedio de tiempos
 double calcularPromedio(nodo* cabeza, int& sumaTiempos) {
     int contador = 0;
     sumaTiempos = 0;
@@ -94,21 +98,15 @@ double calcularPromedio(nodo* cabeza, int& sumaTiempos) {
         actual = actual->siguiente;
     }
 
-    if (contador == 0) return 0.0;
-    return static_cast<double>(sumaTiempos) / contador;
+    return (contador == 0) ? 0.0 : static_cast<double>(sumaTiempos) / contador;
 }
 
-// Función FIFO
+// Implementación de FIFO
 void fifo(nodo* cabeza) {
     nodo* actual = cabeza;
     int tiempoTotal = 0;
     int sumaTiempos = 0;
     double promedio = calcularPromedio(cabeza, sumaTiempos);
-
-    if (actual == nullptr) {
-        cout << "La lista está vacía." << endl;
-        return;
-    }
 
     while (actual != nullptr) {
         tiempoTotal += actual->t;
@@ -122,10 +120,8 @@ void fifo(nodo* cabeza) {
     cout << "Promedio de tiempos: " << promedio << "\n" << endl;
 }
 
-// Función SJF (Shortest Job First)
+// Implementación de SJF
 nodo* seleccionarSJF(nodo* cabeza) {
-    if (cabeza == nullptr) return nullptr;
-
     nodo* menorTiempo = cabeza;
     nodo* actual = cabeza->siguiente;
 
@@ -151,13 +147,10 @@ void ejecutarSJF(nodo*& cabeza) {
     cout << "Promedio de tiempos (SJF): " << promedio << "\n" << endl;
 }
 
-// Función Round Robin
+// Implementación de Round Robin
 void roundRobin(nodo*& cabeza) {
-    int quantum = 7; // Definir un quantum fijo o calcularlo
+    const int quantum = 7;
     int tiempoTotal = 0;
-    int tiemposRetorno[100] = {0};  // Para almacenar los tiempos de retorno de cada proceso
-    int tiemposEspera[100] = {0};   // Para almacenar los tiempos de espera
-    int procesos = 0;
 
     cout << "Quantum definido: " << quantum << endl;
 
@@ -167,42 +160,34 @@ void roundRobin(nodo*& cabeza) {
         while (actual != nullptr) {
             if (actual->t > 0) {
                 int tiempoEjecutado = min(quantum, actual->t);
-                tiemposEspera[actual->id] += tiempoTotal - actual->t_llegada; // Tiempo de espera
-
                 actual->t -= tiempoEjecutado;
                 tiempoTotal += tiempoEjecutado;
-                actual->t_llegada = tiempoTotal;  // Actualizamos la llegada para la próxima ronda
 
-                // Si el proceso ha terminado, calculamos el tiempo de retorno
                 if (actual->t == 0) {
-                    tiemposRetorno[actual->id] = tiempoTotal;
-                    cout << "Proceso ID: " << actual->id << " ha terminado. Tiempo de retorno: " << tiemposRetorno[actual->id] << endl;
-                    procesos++;
+                    cout << "Proceso ID: " << actual->id << " ha terminado. Tiempo total: " << tiempoTotal << endl;
                 }
             }
             actual = actual->siguiente;
         }
     }
 
-    // Calcular el promedio de tiempos de retorno y espera
-    int sumaTiemposEspera = 0;
-    int sumaTiemposRetorno = 0;
-
-    for (int i = 0; i < 100; i++) {
-        if (tiemposRetorno[i] > 0) {
-            sumaTiemposRetorno += tiemposRetorno[i];
-            sumaTiemposEspera += tiemposEspera[i];
-        }
-    }
-
-    cout << "Tiempo promedio de espera (Round Robin): " << (double)sumaTiemposEspera / procesos << endl;
-    cout << "Tiempo promedio de retorno (Round Robin): " << (double)sumaTiemposRetorno / procesos << "\n" << endl;
-
-    // Liberar la lista después de terminar el Round Robin
     liberarLista(cabeza);
 }
 
-// Función de prioridad
+// Implementación de prioridad
+nodo* seleccionarMayorPrioridad(nodo* cabeza) {
+    nodo* mayorPrioridad = cabeza;
+    nodo* actual = cabeza->siguiente;
+
+    while (actual != nullptr) {
+        if (actual->p < mayorPrioridad->p) {
+            mayorPrioridad = actual;
+        }
+        actual = actual->siguiente;
+    }
+    return mayorPrioridad;
+}
+
 void ejecutarPorPrioridad(nodo*& cabeza) {
     int sumaTiempos = 0;
     double promedio = calcularPromedio(cabeza, sumaTiempos);
@@ -216,25 +201,7 @@ void ejecutarPorPrioridad(nodo*& cabeza) {
     cout << "Promedio de tiempos (Prioridad): " << promedio << "\n" << endl;
 }
 
-// Funciones ya existentes para ejecutar y remover
-nodo* seleccionarMayorPrioridad(nodo* cabeza) {
-    if (cabeza == nullptr) return nullptr;
-
-    nodo* mayorPrioridad = cabeza;
-    nodo* actual = cabeza->siguiente;
-
-    while (actual != nullptr) {
-        if (actual->p < mayorPrioridad->p) {
-            mayorPrioridad = actual;
-        }
-        actual = actual->siguiente;
-    }
-    return mayorPrioridad;
-}
-
 void ejecutarYRemoverProceso(nodo*& cabeza, nodo* proceso) {
-    if (cabeza == nullptr || proceso == nullptr) return;
-
     if (cabeza == proceso) {
         cabeza = cabeza->siguiente;
     } else {
@@ -245,10 +212,9 @@ void ejecutarYRemoverProceso(nodo*& cabeza, nodo* proceso) {
         actual->siguiente = proceso->siguiente;
     }
 
-    cout << "Ejecutando proceso ID: " << proceso->id << " con prioridad " << proceso->p << endl;
+    cout << "Ejecutando proceso ID: " << proceso->id << endl;
     delete proceso;
 }
-
 
 // Main
 int main() {
@@ -257,44 +223,45 @@ int main() {
     int opcion;
 
     do {
-        cout << "Ingresa una opcion: \n";
-        cout << "Ingresar valores-[1]\n";
-        cout << "FIFO-[2]\n";
-        cout << "SJF-[3]\n";
-        cout << "Round Robin-[4]\n";
-        cout << "Prioridad-[5]\n";
-        cout << "Salir-[0]\n";
+        cout << "Ingrese una opción:\n";
+        cout << "1. Ingresar valores\n";
+        cout << "2. FIFO\n";
+        cout << "3. SJF\n";
+        cout << "4. Round Robin\n";
+        cout << "5. Prioridad\n";
+        cout << "0. Salir\n";
         cin >> opcion;
 
         switch (opcion) {
             case 1:
-                pedirdatos(datos);
-            break;
+                pedirDatos(datos);
+                break;
             case 2:
-                tranferirDatos(datos, cabeza);
+                transferirDatos(datos, cabeza);
                 fifo(cabeza);
-                liberarLista(cabeza);  // Liberar después de FIFO
-            break;
+                liberarLista(cabeza);
+                break;
             case 3:
-                tranferirDatos(datos, cabeza);
+                transferirDatos(datos, cabeza);
                 ejecutarSJF(cabeza);
-                liberarLista(cabeza);  // Liberar después de SJF
-            break;
+                liberarLista(cabeza);
+                break;
             case 4:
-                tranferirDatos(datos, cabeza);
-                roundRobin(cabeza);  // La función ya incluye la liberación de la lista
-            break;
+                transferirDatos(datos, cabeza);
+                roundRobin(cabeza);
+                break;
             case 5:
-                tranferirDatos(datos, cabeza);
+                transferirDatos(datos, cabeza);
                 ejecutarPorPrioridad(cabeza);
-                liberarLista(cabeza);  // Liberar después de Prioridad
-            break;
+                liberarLista(cabeza);
+                break;
             case 0:
-                cout << "Saliendo del programa." << endl;
-            break;
+                cout << "Saliendo del programa.\n";
+                break;
             default:
-                cout << "Opción no válida. Inténtalo de nuevo." << endl;
-            break;
+                cout << "Opción no válida.\n";
         }
     } while (opcion != 0);
+
+    return 0;
 }
